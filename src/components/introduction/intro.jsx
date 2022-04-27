66374bf5f360cdc56321b7e231cfc942520ec5b0
leo carvalho <lndrcrvlh@gmail.com>
2022-04-26 20:12:01 -0400

    Adds basic Styling

--- src/components/introduction/intro.jsx
+++ src/components/introduction/intro.jsx
@@ -1,8 +1,14 @@
+import "./intro.css"
 
 const Intro = () => {
     return (
-        <div>
-            Hello, I'm Leandro Carvalho
+        <div className="intro">
+            <div className="intro-left">
+                <div className="intro-wrapper">
+                    <h1 class="intro-heading">I'm Leandro Carvalho</h1>
+                    <h2 class="intro-subheading">a developer</h2></div>
+            </div>
+            <div className="intro-right">right</div>
         </div>
     );
 };
