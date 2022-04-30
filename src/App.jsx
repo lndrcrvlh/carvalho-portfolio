f99e74413dfd3421ce535dd3b0f12c1010ce3bbf
leo carvalho <lndrcrvlh@gmail.com>
2022-04-30 11:26:44 -0400

    Initializes Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Intro from "./components/introduction/intro";
 import About from "./components/about/about"
 import Contact from "./components/contact/contact";
+import Projects from "./components/projects/projects";
 
 const App = () => {
   return (
     <div>
       <Intro />
       <About />
+      <Projects />
       <Contact />
     </div>
     )
---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './projects.css'
+
+function Projects() {
+  return (
+    <div className='projects'>Projects
+    
+    </div>
+  )
+}
+
+export default Projects
