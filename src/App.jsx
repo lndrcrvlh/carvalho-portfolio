0a0f20213d719ed85e286638880e71f78bbf68d6
leo carvalho <lndrcrvlh@gmail.com>
2022-04-30 11:23:06 -0400

    Initializes contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Intro from "./components/introduction/intro";
 import About from "./components/about/about"
+import Contact from "./components/contact/contact";
 
 const App = () => {
   return (
     <div>
       <Intro />
       <About />
+      <Contact />
     </div>
     )
 };
---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>Contact Form</div>
+  )
+}
+
+export default Contact
