637d6ebe25f3e11e56a008f0cbdb0ab3889d07a5
leo carvalho <lndrcrvlh@gmail.com>
2022-04-30 11:17:44 -0400

    Rearranges skills icons

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,15 +8,12 @@ const About = () => {
       <div className="about-left">
         <div className="about-skills">
           <div className="about-skills-wrapper">
-            <img src={IMAGES.Canva} alt="canva" className="skill-logo" />
-            <img src={IMAGES.CSS} alt="CSS" className="skill-logo" />
-            <img
-              src={IMAGES.Elementor}
-              alt="Elementor"
-              className="skill-logo"
-            />
-            <img src={IMAGES.GIMP} alt="GIMP" className="skill-logo" />
             <img src={IMAGES.git} alt="git" className="skill-logo" />
+            <img src={IMAGES.rust} alt="Rust" className="skill-logo" />
+            <img src={IMAGES.nodeJS} alt="NodeJS" className="skill-logo" />
+            <img src={IMAGES.CSS} alt="CSS" className="skill-logo" />
+
+            <img src={IMAGES.react} alt="React" className="skill-logo" />
             <img src={IMAGES.html} alt="HTML" className="skill-logo" />
             <img
               src={IMAGES.javascript}
@@ -24,19 +21,23 @@ const About = () => {
               className="skill-logo"
             />
             <img src={IMAGES.mongoDB} alt="mongoDB" className="skill-logo" />
+            <img
+              src={IMAGES.wordpress}
+              alt="WordPress"
+              className="skill-logo"
+            />
             <img src={IMAGES.mysql} alt="mySQL" className="skill-logo" />
-            <img src={IMAGES.nodeJS} alt="NodeJS" className="skill-logo" />
             <img src={IMAGES.php} alt="PHP" className="skill-logo" />
-            <img src={IMAGES.react} alt="React" className="skill-logo" />
-            <img src={IMAGES.rust} alt="Rust" className="skill-logo" />
             <img
               src={IMAGES.ubuntu}
               alt="Ubuntu/Linux"
               className="skill-logo"
             />
+            <img src={IMAGES.GIMP} alt="GIMP" className="skill-logo" />
+            <img src={IMAGES.Canva} alt="canva" className="skill-logo" />
             <img
-              src={IMAGES.wordpress}
-              alt="WordPress"
+              src={IMAGES.Elementor}
+              alt="Elementor"
               className="skill-logo"
             />
           </div>
