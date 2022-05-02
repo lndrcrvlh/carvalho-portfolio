26a77d366ac9beb9c39afe2dc1eaad0ca957fe0d
leo carvalho <lndrcrvlh@gmail.com>
2022-05-02 17:44:12 -0400

    Removes unused code

--- src/components/tabs/projectsTabs.tsx
+++ src/components/tabs/projectsTabs.tsx
@@ -1,55 +1,15 @@
 import * as React from "react";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-
 import TabsUnstyled from "@mui/base/TabsUnstyled";
 import TabsListUnstyled from "@mui/base/TabUnstyled";
 import TabUnstyled from "@mui/base/TabUnstyled";
 import TabPanelUnstyled from "@mui/base/TabPanelUnstyled";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 
 export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
 
   return (
     <TabsUnstyled defaultValue={0}>
-      <TabsListUnstyled>
+      <TabsListUnstyled className="tabs-buttons">
         <TabUnstyled value={0}>One</TabUnstyled>
         <TabUnstyled value={1}>Two</TabUnstyled>
         <TabUnstyled value={2}>Three</TabUnstyled>
