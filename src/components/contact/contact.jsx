1eb0371080696b6fb7616ba52ddf999c1e36b8a8
leo carvalho <lndrcrvlh@gmail.com>
2022-05-03 16:53:52 -0400

    Adds basic structure to contacts component

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div className='contact'>Contact Form</div>
-  )
-}
+    <div className="contact">
+      <a href="mailto:lndrcrvlh@gmail.com" className="email-button">Send Me an Email</a>
+      
+      <hr className="divider"></hr>
+      <div className="social-links">
+        <a href="/" className="github">
+          github logo
+        </a>
+        <a href="/" className="linkedin">
+          linkedin logo
+        </a>
+        <a href="/" className="medium">
+          medium logo
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
