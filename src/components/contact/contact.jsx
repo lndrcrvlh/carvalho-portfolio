a358983e394558ab882322e14e518b07b6f85d9b
lndrcrvlh <lndrcrvlh@gmail.com>
2022-10-14 11:53:59 -0400

    Adds resume

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./contact.css";
-import linkedin from "../../images/logos/linkedinLogo.png"
-import github from "../../images/logos/githubLogo.png"
-import medium from "../../images/logos/mediumLogo.jpeg"
+import linkedin from "../../images/logos/linkedinLogo.png";
+import github from "../../images/logos/githubLogo.png";
+import medium from "../../images/logos/mediumLogo.jpeg";
+import Resume from "../../assets/Resume.pdf";
 
 const Contact = () => {
   return (
     <div className="contact">
-      <a href="mailto:lndrcrvlh@gmail.com" className="email-button">Send Me an Email</a>
-      
+      <a href="mailto:lndrcrvlh@gmail.com" className="contact-button">
+        Send Me an Email
+      </a>
+
+      <a
+        href={Resume}
+        download="Resume"
+        target="_blank"
+        className="contact-button"
+      >
+        Download My Resume
+      </a>
+
       <hr className="divider"></hr>
       <div className="social-links">
         <a href="https://github.com/lndrcrvlh" className="github">
@@ -18,7 +30,7 @@ const Contact = () => {
           <img src={linkedin} alt="linkedin" className="logo" />
         </a>
         <a href="https://teixeiras.medium.com/" className="medium">
-        <img src={medium} alt="medium" className="logo" />
+          <img src={medium} alt="medium" className="logo" />
         </a>
       </div>
     </div>
